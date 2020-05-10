70dd5dc2099b8bd521feaa6ec14b88b28ca70463
fran <fran@pop-os.localdomain>
2020-05-10 19:26:23 +0200

    semver

--- scripts/semver.js
+++ scripts/semver.js
@@ -1,4 +1,17 @@
 #! /usr/bin/env node
 
+const fs = require('fs')
+const path = require('path')
+const semver = require('semver')
+
 const parts = process.env.GITHUB_REF.split(/\//)
-console.log(parts)
+
+const source = fs.readFileSync(path.join(__dirname, '../package.json'))
+
+const content = JSON.parse(source.toString())
+
+if (parts[0] === 'refs' && parts[1] === 'heads') {
+  if (parts[2] === 'feature') {
+    console.log(semver.inc(content.version, 'minor'))
+  }
+}
