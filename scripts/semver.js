23210867690e00fbea013b8aee67ddf3e1f6afed
fran <fran@pop-os.localdomain>
2020-05-14 15:08:52 +0200

    debug branch

--- scripts/semver.js
+++ scripts/semver.js
@@ -1,7 +1,6 @@
 #! /usr/bin/env node
 
 const fs = require('fs')
-const child_process = require('child_process')
 const path = require('path')
 const semver = require('semver')
 const core = require('@actions/core')
@@ -15,13 +14,13 @@ const content = JSON.parse(source.toString())
 let nextVersion = content.version
 
 if (parts[0] === 'refs' && parts[1] === 'heads') {
+  core.debug(`Branch is ${ parts[2] }`)
   if (parts[2] === 'feature') {
     nextVersion = semver.inc(content.version, 'preminor')
   } else if (parts[2] === 'fix') {
     nextVersion = semver.inc(content.version, 'prepatch')
   } else if (parts[2] === 'develop') {
     nextVersion = semver.inc(content.version, 'release')
-    child_process.execSync(`git tag`)
   }
 }
 
